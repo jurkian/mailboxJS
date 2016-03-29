be07783cf1cfa17e5cb282129a3c3f24a22dd847
Jakub Jurkian <jakub@jurkian.com>
2016-03-29 10:34:00 +0200

    Add truncate filter to limit email title's length

---
+++ app/scripts/filters/truncate.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('mailboxApp')
+  .filter('truncate', function () {
+    return function (input, max, tail) {
+
+      if (!input) {
+      	return '';
+      }
+
+      if (!max) {
+      	return input;
+      }
+
+      max = parseInt(max);
+
+      if (input.length <= max) {
+      	return input;
+      }
+
+      input = input.substr(0, max);
+
+      return input + (tail || '…');
+    };
+  });
